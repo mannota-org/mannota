20a0ae2b48b85377d3625a0737fcd97e80a7da54
Phạm Quỳnh Trang <trangpq2310@gmail.com>
2024-11-29 12:03:25 +0700

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  redirect("/dashboard"); // Redirects to the /dashboard route
+  redirect("/dashboard"); 
   return null; 
 }
